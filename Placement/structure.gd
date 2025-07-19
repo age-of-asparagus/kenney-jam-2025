35df2f5ac44c8cae5f71e18c67bb026179b72acc
Tylere Couture <tylere.couture@gmail.com>
2025-07-19 16:50:58 -0700

    Add icon and name to structures

--- Placement/structure.gd
+++ Placement/structure.gd
@@ -1,7 +1,10 @@
 extends Resource
 class_name Structure
 
+@export var name := ""
 @export var scene : PackedScene 
 @export var cost : int
 @export var energyUse : int
 @export var target_placement : bool
+
+@export var icon : Texture2D
