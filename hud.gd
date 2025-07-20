2b13135804b9285c1a7969c74ff08e20838bd61e
Tylere Couture <tylere.couture@gmail.com>
2025-07-20 01:03:11 -0700

    improved eneergy bar

--- hud.gd
+++ hud.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 @onready var structure_selector: ItemList = $MarginContainer/StructureSelector
-
+@onready var energy_available: Label = $MarginContainer/VBoxContainer/HBoxContainer2/Energy_Available
+@onready var energy_used: Label = $MarginContainer/VBoxContainer/HBoxContainer2/Energy_Used
+@onready var energy_progress_bar: ProgressBar = $MarginContainer/VBoxContainer/HBoxContainer2/EnergyProgressBar
 @export var structures: Array[Structure] = []
 
 signal structure_selected
@@ -11,8 +13,10 @@ func _ready():
 func _physics_process(delta):
 	Global.cash += 5
 	$MarginContainer/HBoxContainer/Cash.text = "$"+str(Global.cash)
-	$MarginContainer/HBoxContainer2/Energy_Available.text = str(Global.total_energy-Global.energy_used)
-	
+	var percent = 100*Global.energy_used/Global.total_energy
+	energy_available.text = str(Global.total_energy) + " MW"
+	energy_used.text = str(Global.energy_used) + " MW (" + str(percent) +"%)"
+	energy_progress_bar.value = percent
 	update_inventory(Global.cash)
 	
 func update_inventory(cash : int):
