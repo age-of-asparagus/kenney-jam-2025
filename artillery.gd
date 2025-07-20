13b327fe016396fb97717da5f6a8c76110dfb960
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2025-07-19 19:42:24 -0700

    artillery start

--- artillery.gd
+++ artillery.gd
@@ -6,7 +6,7 @@ extends Node3D
 var health : int
 @export var firerate : float
 
-#var projectile = preload()
+var Cannonball = preload("res://cannonball.tscn")
 
 
 @export var on := false
@@ -30,23 +30,16 @@ func _physics_process(delta):
 			queue_free()
 
 func shoot():
-	var Bullet = bullet.instantiate()
-	get_next_bullet_position()
-	get_tree().current_scene.add_child(Bullet)
-	Bullet.global_position = bullet_position[bullet_position_index].global_position
-	Bullet.rotation = rotation
+	var Cannonball = Cannonball.instantiate()
+	get_tree().current_scene.add_child(Cannonball)
+	Cannonball.global_position = $Marker3D.global_position
+	Cannonball.rotation = rotation
 	if enemy:
-		Bullet.get_node("Area3D").set_collision_layer_value(1,false)
-		Bullet.get_node("Area3D").set_collision_layer_value(2,true)
-		Bullet.get_node("Area3D").set_collision_mask_value(1,true)
-		Bullet.get_node("Area3D").set_collision_mask_value(2,false)
+		Cannonball.get_node("Area3D").set_collision_layer_value(1,false)
+		Cannonball.get_node("Area3D").set_collision_layer_value(2,true)
+		Cannonball.get_node("Area3D").set_collision_mask_value(1,true)
+		Cannonball.get_node("Area3D").set_collision_mask_value(2,false)
 
-
-
-func get_next_bullet_position():
-	bullet_position_index += 1
-	if bullet_position_index == bullet_position.size():
-		bullet_position_index = 0
 
 func _on_timer_timeout():
 	if on:
