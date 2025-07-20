cb656fbf535d13d5f51435fa61f80d3060531d86
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2025-07-19 23:30:34 -0700

    Update arena.gd

--- arena.gd
+++ arena.gd
@@ -22,7 +22,7 @@ func _on_builder_structure_placed() -> void:
 	$HUD.deselect_structure()
 
 
-func get_random_enemy_location() -> Vector3:
+func get_random_friendly_location() -> Vector3:
 	# All player units  placed are added to the unit container
 	var children = unit_container.get_children()
 	# Get a random enemy (if there is any)
@@ -31,30 +31,30 @@ func get_random_enemy_location() -> Vector3:
 		return random_child.global_position
 	else:
 		# pick a random cell on the grid:
-		return $GridMap.get_random_location(true)
+		return $GridMap.get_random_location(false)
 
 func spawn_power():
 	var Power = power.instantiate()
 	add_child(Power)
-	Power.position = get_random_enemy_location()
+	Power.position = get_random_friendly_location()
 	Power.on = true
 	Power.start()
 func spawn_turret():
 	var Turret = turret.instantiate()
 	add_child(Turret)
-	Turret.position = get_random_enemy_location()
+	Turret.position = get_random_friendly_location()
 	Turret.on = true
 	Turret.start()
 func spawn_bank():
 	var Bank = bank.instantiate()
 	add_child(Bank)
-	Bank.position = get_random_enemy_location()
+	Bank.position = get_random_friendly_location()
 	Bank.on = true
 	Bank.start()
 func spawn_artillery():
 	var Artillery = artillery.instantiate()
 	add_child(Artillery)
-	Artillery.position = get_random_enemy_location()
+	Artillery.position = get_random_friendly_location()
 	Artillery.on = true
 	Artillery.start()
 
