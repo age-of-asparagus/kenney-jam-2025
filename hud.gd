4b88e8afad646dc64ebdbaa78d59e37a919fe588
Tylere Couture <tylere.couture@gmail.com>
2025-07-19 18:26:40 -0700

    HUD structure selector

--- hud.gd
+++ hud.gd
@@ -13,3 +13,7 @@ func set_inventory(structures:Array[Structure]) -> void:
 	for structure in structures:
 		# add them all to the selector widget
 		structure_selector.add_item(structure.name, structure.icon)
+
+
+func _on_structure_selector_item_selected(index: int) -> void:
+	pass # Replace with function body.
